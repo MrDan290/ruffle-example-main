8debb740429bbcee4cd60bbb1f26e410a4bd23af
MrDan290 <73743656+MrDan290@users.noreply.github.com>
2024-09-06 19:15:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ swfobject.embedSWF = function(url, cont, width, height){
             style: 'width: ' + width + 'px; height: ' + height + 'px',
         });
 
+    player.style.width = "100vw"; 
+    player.style.height = "100vh";
     player.load({ url: url });
 }
 
